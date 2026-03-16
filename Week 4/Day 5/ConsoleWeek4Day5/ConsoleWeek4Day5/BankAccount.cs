bc1c6ca6b1332556186f1b987e0a0d345d70eaf4
vinayreddy818 <vinaykumarreddykusuma@gmail.com>
2026-03-16 13:22:08 +0530

    .

---
+++ Week 4/Day 5/ConsoleWeek4Day5/ConsoleWeek4Day5/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleWeek4Day5
+{
+    internal class BankAccount
+    {
+        private double balance = 0;
+        public void deposit(double depositAmount)
+        {
+            balance += depositAmount;
+        }
+        public void withDraw(double withDrawAmount) 
+        {
+            if (withDrawAmount > balance)
+            {
+                Console.WriteLine("Insufficient balance");
+            }
+            else
+            {
+                balance -= withDrawAmount;
+
+            }
+        }
+        public double getBalance() 
+        {
+            return balance;
+        }
+
+    }
+}
---
+++ Week 4/Day 5/ConsoleWeek4Day5/ConsoleWeek4Day5/BankAccountEncapsulation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleWeek4Day5
+{
+    internal class BankAccountEncapsulation
+    {
+        static void Main()
+        {
+            BankAccount b = new BankAccount();
+            Console.WriteLine("Enter Deposit Amount");
+            double depositAmount = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Withdraw Amount");
+            double withDrawAmount = Convert.ToDouble(Console.ReadLine());
+            b.deposit(depositAmount);
+            b.withDraw(withDrawAmount);
+            Console.WriteLine($"Current Balance : {b.getBalance()}");
+
+        }
+    }
+}
